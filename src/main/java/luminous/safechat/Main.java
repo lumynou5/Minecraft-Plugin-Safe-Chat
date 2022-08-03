67c16880092380752cf749bc38e282292fd2affe
Luminous-Coder <luminous.coder@gmail.com>
2022-08-03 16:24:23 +0800

    feat: Add message re-sending

---
+++ src/main/java/luminous/safechat/Main.java
@@ -0,0 +1,23 @@
+package luminous.safechat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+    }
+}
+
+class ChatListener implements Listener {
+    @EventHandler
+    public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
+        String message = event.getMessage();
+        event.setCancelled(true);
+        Bukkit.broadcastMessage(message);
+    }
+}
